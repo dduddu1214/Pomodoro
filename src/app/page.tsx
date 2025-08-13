d74e8be421b2d4e0942b6e5ed210375ad1b58835
dduddu1214 <tmfrl1747@naver.com>
2025-08-13 13:45:09 +0900

    ✨ Add SEO metadata and social sharing optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
 import PomodoroTimer from '@/components/PomodoroTimer';
 
 export default function Home() {
-  return (
-    <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link 
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500;600&display=swap" 
-        rel="stylesheet" 
-      />
-      <link 
-        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" 
-        rel="stylesheet" 
-      />
-      <PomodoroTimer />
-    </>
-  );
+  return <PomodoroTimer />;
 }
